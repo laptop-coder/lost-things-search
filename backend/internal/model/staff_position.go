0922d985fd268dab719b372c63c4f5c529a9e476
laptop-coder <coderlaptop@gmail.com>
2026-05-05 11:38:05 +0300

    feat(backend): increase position size to 200 bytes

--- backend/internal/model/staff_position.go
+++ backend/internal/model/staff_position.go
@@ -8,7 +8,7 @@ type StaffPosition struct {
 	ID        uint16 `gorm:"primaryKey"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	Name      string `gorm:"type:varchar(100);unique;check:length(trim(name)) >= 4"`
+	Name      string `gorm:"type:varchar(200);unique;check:length(trim(name)) >= 4"`
 	// 1. Can't remove position if there are at least one person with it
 	// 2. one-to-many (position-to-staff)
 	Staff []Staff `gorm:"foreignKey:PositionID;references:ID;constraint:OnDelete:restrict,OnUpdate:restrict"`
