94de64042e680d728d6708b5be15f6ae6e067f0c
laptop-coder <coderlaptop@gmail.com>
2026-07-16 13:11:13 +0300

    ci: run migrations on update

--- scripts/get_updates_and_restart.sh
+++ scripts/get_updates_and_restart.sh
@@ -7,6 +7,8 @@ docker compose pull backend migrate ml frontend
 NEW=$(docker compose images -q backend migrate ml frontend)
 
 if [ "$OLD" != "$NEW" ]; then
+    make migrate
+    docker rm lts-migrate
     make down
     make deploy
 fi
